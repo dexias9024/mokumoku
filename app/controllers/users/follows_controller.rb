bbd596bd53b9d2db4b938addf99d37fae2e6039f
42-hoshino-M <xingyem9024@gmail.com>
2023-09-28 20:11:14 +0900

    graduation_test

--- app/controllers/users/follows_controller.rb
+++ app/controllers/users/follows_controller.rb
@@ -1,12 +1,17 @@
 class Users::FollowsController < ApplicationController
+  before_action :user_paramus
+
   def create
-    @user = User.find(params[:user_id])
     Follow.create(follower_id: current_user.id, followed_id: params[:user_id])
   end
   
   def destroy
-    @user = User.find(params[:user_id])
     following = Follow.find_by(follower_id: current_user.id, followed_id: params[:user_id])
     following.destroy
+  end
+
+  private
+  def user_paramus
+    @user = User.find(params[:user_id])
   end
 end
